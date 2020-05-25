f0efc9a4597b992202f23656b4366bf6b597fac0
Mikhail-Meshcheriakov <sekesh@mail.ru>
2020-05-26 06:22:15 +0700

    Код 3-го урока

--- core/src/com/dune/game/core/Projectile.java
+++ core/src/com/dune/game/core/Projectile.java
@@ -6,31 +6,44 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
 
-public class Projectile {
-    private Vector2 position;
-    private Vector2 velocity;
+public class Projectile extends GameObject implements Poolable {
     private TextureRegion texture;
+    private Vector2 velocity;
+    private float speed;
+    private float angle;
+    private boolean active;
 
-    public Projectile(TextureAtlas atlas) {
-        this.texture = new TextureRegion(atlas.findRegion("bullet"));
-        this.position = new Vector2(-100.0f, -100.0f);
-        velocity = new Vector2(0, 0);
+    @Override
+    public boolean isActive() {
+        return active;
     }
 
-    public void setup(Vector2 startPosition, float angle) {
-        position.set(startPosition);
-        velocity.set(500.0f * MathUtils.cosDeg(angle), 500.0f * MathUtils.sinDeg(angle));
+    public void deactivate() {
+        active = false;
     }
 
-    public void update(float dt) {
-        position.mulAdd(velocity, dt);
+    public Projectile(GameController gc) {
+        super(gc);
+        this.velocity = new Vector2();
+        this.speed = 320.0f;
+    }
+
+    public void setup(Vector2 startPosition, float angle, TextureRegion texture) {
+        this.texture = texture;
+        this.position.set(startPosition);
+        this.angle = angle;
+        this.velocity.set(speed * MathUtils.cosDeg(angle), speed * MathUtils.sinDeg(angle));
+        this.active = true;
     }
 
     public void render(SpriteBatch batch) {
         batch.draw(texture, position.x - 8, position.y - 8);
     }
 
-    public Vector2 getPosition() {
-        return position;
+    public void update(float dt) {
+        position.mulAdd(velocity, dt);
+        if (position.x < 0 || position.x > 1280 || position.y < 0 || position.y > 720) {
+            deactivate();
+        }
     }
 }
